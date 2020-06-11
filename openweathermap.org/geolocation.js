d51cb115df0173152306598802827c9521603bb6
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-10 21:30:52 -0400

    used geolocation api to get coordinates of where someone is

--- openweathermap.org/geolocation.js
+++ openweathermap.org/geolocation.js
@@ -1,5 +1,24 @@
-document.getElementById("geolocation").addEventListener("click", geolocator);
+document.getElementById("geolocation").addEventListener("click", geolocate);
 
-function geolocator() {
-    
+function geolocate() {
+    if (window.navigator && window.navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
+    }
+}
+
+function onGeolocateSuccess(coordinates) {
+    const {latitude, longitude} = coordinates.coords;
+    console.log(`Found coordinates: ${latitude}, ${longitude}`)
+}
+
+function onGeolocateError(error) {
+    console.warn(error.code, error.message);
+
+    if(error.code === 1) {
+        //they said they won't let you use location
+    } else if (error.code === 2) {
+        //position is unavailable
+    } else if (error.code === 3) {
+        //timeout
+    }
 }
