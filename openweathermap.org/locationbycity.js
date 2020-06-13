578bbfb976fd8c78619ea57bb844224d6143bc14
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-13 19:52:25 -0400

    created response text object with parsed JS

--- openweathermap.org/locationbycity.js
+++ openweathermap.org/locationbycity.js
@@ -1,4 +1,18 @@
 
+document.getElementById("submit").addEventListener("click", () => {
+    const cityName = document.getElementById("city").value
+    console.log(document.getElementById("city").value)
+    const apiKey = "28b070462390d4739c7761be2c99d179"
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = function() {
+        if (this.status === 200 && this.readyState === 4) {
+            document.getElementById("response-text").innerHTML = this.responseText;
+        }
+    }
+    xhr.send()
+    document.getElementById("city").value = "";
+})
 
-url = `api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${yourapikey}`
 
