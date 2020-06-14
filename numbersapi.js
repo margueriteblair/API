46026cd55a685a275d1e162889db54dccb6715ef
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-14 16:40:17 -0400

    randomized the facts according to type

--- numbersapi.js
+++ numbersapi.js
@@ -1,5 +1,14 @@
 document.getElementById("button").addEventListener("click", () => {
-    let types = ["triva", "math", "date", "year"]
+    let typesArray = ["triva", "math", "date", "year"]
+    let numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    // typesArray.sort(function() {
+    //     console.log(.5 - Math.random());
+    //     console.log(typesArray);
+    // });
+    let type = typesArray[Math.floor(Math.random()*typesArray.length)];
+    let number = numbersArray[Math.floor(Math.random()*numbersArray.length)];
+    // console.log(typesArray[(Math.floor(Math.random()*typesArray.length))]);
+    // console.log(numbersArray[Math.random()*numbersArray.length])
     let url = `http://numbersapi.com/${number}/${type}`
     xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
