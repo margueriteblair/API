8f08c9c29c0aefae4a326d8c75b683a3989768d7
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-16 18:30:18 -0400

    added reverse geocoding API

--- geolocationbycity.js
+++ geolocationbycity.js
@@ -7,10 +7,23 @@ function geolocate() {
 function success(position) {
     let longitude = position.coords.longitude;
     let latitude = position.coords.latitude;
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${YOUR_API_KEY}`
     console.log(`${longitude}, ${latitude}`)
     console.log(position)
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = function() {
+        if (this.status === 200 && this.readyState === 4){
+            console.log()
+        } else if (this.status === 404){
+            console.log(`Error! 404.`)
+        }
+    }
 }
 
 function error(position) {
     console.warn(`help`)
 }
+
+// https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452
+// &location_type=ROOFTOP&result_type=street_address&key=YOUR_API_KEY
