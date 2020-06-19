5b2e7844041909dad97401e2f83abf4966feb42e
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-19 00:55:46 -0400

    replaced bing api with zenserp

--- geolocationbycity.js
+++ geolocationbycity.js
@@ -18,27 +18,15 @@ function success(position) {
             let jsonParseResult = JSON.parse(this.responseText)
             console.log(jsonParseResult.results[6].formatted_address) //check if this is standard across all locations;
             document.getElementById("output").innerHTML = jsonParseResult.results[6].formatted_address
+            let location = jsonParseResult.results[6].formatted_address;
+            let apiKey2 = "388752b0-b1e7-11ea-8ea7-3dd0050eb327"
+            let url2 = `https://app.zenserp.com/api/v2/search?apikey=${apiKey2}&q=${location}&tbm=isch&device=desktop&location=Manhattan,New%20York,United%20States`
             let xhr2 = new XMLHttpRequest();
-            xhr2.open("GET", `https://serpapi.com/playground?q=${jsonParseResult.results[6].formatted_address}&tbm=isch&ijn=0`, true);
+            xhr2.open("GET", url2, true);
             xhr2.onload = function() {
                 if (this.status === 200 && this.readyState === 4){
-                    // console.log(this.responseText)
-                    // console.log(JSON.parse(this.responseText))
+                    console.log(JSON.parse(this.responseText))
                     // document.getElementById("img").innerHTML = this.responseText;
-                    // 'use strict';
-                    // let https = require('https');
-                    // let subscriptionKey = "";
-                    // let host = 'api.cognitive.microsoft.com';
-                    // let path = '/bing/v7.0/images/search';
-                    // let term = `${jsonParseResult.results[6].formatted_address}`;
-
-                    // let request_params = {
-                    //     method: 'GET',
-                    //     hostname: host,
-                    //     path: path: {
-                    //         'Ocp-Apim-Subscription-Key': subscriptionKey,
-                    //     }s
-                    // };
                 } else if (this.status === 404) {
                     console.log(`Error! 404 Status.`)
                 }
